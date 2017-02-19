f58931bff36095ecc55835a5d2c9f6324b916548
Edwin Diaz <edwinddiazd@gmail.com>
2017-02-19 17:35:20 -0400

    carga del css y js

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends ('main')
+@section ('content')
+contenido home en el beta esta verga no ve
+@stop
---
+++ resources/views/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Beta</title>
+	<link rel="stylesheet" href="material.min.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body>
+@include('partials._navbar')
+<main class="mdl-layout__content">
+    <div class="page-content">
+    @yield ('content')<!-- Your content goes here --></div>
+  </main>
+	
+
+</body>
+<script src="material.min.js"></script>
+</html>
