13746e16eada9274dc0a6cb576b0684a6cd6439b
madeleind <madeleind.reyes@gmail.com>
2017-02-23 16:17:53 -0400

    modificacion vista personal y producto

--- resources/views/Personal.blade.php
+++ resources/views/Personal.blade.php
@@ -15,9 +15,9 @@
 
 	<div class="mdl-cell mdl-cell--8-col">
 		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
-		    <input class="mdl-textfield__input" type="text" id="sample1">
-		    <label class="mdl-textfield__label" for="sample1">DIRECCION</label>
+		  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+		    <textarea class="mdl-textfield__input" type="text" rows= "1" id="sample5" ></textarea>
+		    <label class="mdl-textfield__label" for="sample5">Dirección</label>
 		  </div>
 		</form>
 	</div>
@@ -125,15 +125,26 @@
 	<div class="mdl-cell mdl-cell--3-col">
 		<form action="#">
 		  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-		    <input class="mdl-textfield__input" type="text" id="sample3">
-		    <label class="mdl-textfield__label" for="sample3">Encargado de Ventas</label>
+		    <select style="margin-left: -15px; border-color: blue;">
+		    	<option>Selecciones el cargo</option>
+		    	<option>Encargado Ventas</option>
+		    	<option>Encargado Compras</option>
+		    	<option>Encargado Juridico</option>
+		    	<option>Encargado Operaciones</option>
+		    	<option>Encargado Seguridad Integral</option>
+		    	<option>Encargado Seguridad Industrial</option>
+		    	<option>Encargado Bienes</option>
+		    	<option>Encargado Gestión Humana</option>
+		    	<option>Encargado Tecnología</option>
+		    	<option>Encargado Comunicaciones</option>
+		    </select>
 		  </div>
 		</form>
 	</div>
 
 	<div class="mdl-cell mdl-cell--3-col">
 		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
+		  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
 		    <input class="mdl-textfield__input" type="text" id="sample1">
 		    <label class="mdl-textfield__label" for="sample1">NOMBRE</label>
 		  </div>
@@ -142,7 +153,7 @@
 
 	<div class="mdl-cell mdl-cell--3-col">
 		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
+		  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
 		    <input class="mdl-textfield__input" type="text" id="sample1">
 		    <label class="mdl-textfield__label" for="sample1">APELLIDO</label>
 		  </div>
@@ -150,9 +161,8 @@
 	</div>
 
 	<div class="mdl-cell mdl-cell--3-col">
-		<!-- Numeric Textfield -->
 		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
+		  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
 		    <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">
 		    <label class="mdl-textfield__label" for="sample2">CEDULA</label>
 		    <span class="mdl-textfield__error">Input is not a number!</span>
@@ -174,7 +184,7 @@
 
 	<div class="mdl-cell mdl-cell--3-col">
 		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
+		  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
 		    <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">
 		    <label class="mdl-textfield__label" for="sample2">TELEFONO N° 1</label>
 		    <span class="mdl-textfield__error">Input is not a number!</span>
@@ -184,7 +194,7 @@
 
 	<div class="mdl-cell mdl-cell--3-col">
 		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
+		  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
 		    <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">
 		    <label class="mdl-textfield__label" for="sample2">TELEFONO N°2</label>
 		    <span class="mdl-textfield__error">Input is not a number!</span>
@@ -193,219 +203,10 @@
 	</div>
 </div>
 
-<div class="mdl-grid">
-	<div class="mdl-cell mdl-cell--3-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-		    <input class="mdl-textfield__input" type="text" id="sample3">
-		    <label class="mdl-textfield__label" for="sample3">Encargado de Compras</label>
-		  </div>
-		</form>
-	</div>
-
-	<div class="mdl-cell mdl-cell--3-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
-		    <input class="mdl-textfield__input" type="text" id="sample1">
-		    <label class="mdl-textfield__label" for="sample1">NOMBRE</label>
-		  </div>
-		</form>
-	</div>
-
-	<div class="mdl-cell mdl-cell--3-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
-		    <input class="mdl-textfield__input" type="text" id="sample1">
-		    <label class="mdl-textfield__label" for="sample1">APELLIDO</label>
-		  </div>
-		</form>
-	</div>
-
-	<div class="mdl-cell mdl-cell--3-col">
-		<!-- Numeric Textfield -->
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
-		    <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">
-		    <label class="mdl-textfield__label" for="sample2">CEDULA</label>
-		    <span class="mdl-textfield__error">Input is not a number!</span>
-		  </div>
-		</form>
+<div class="mdl-grid"
+	<div class="mdl-cell mdl-cell--4-col-offset mdl-cell--4-col">
+		<button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored">Agregar</button>
 	</div>
 </div>
 
-
-<div class="mdl-grid">
-	<div class="mdl-cell mdl-cell--6-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-		    <input class="mdl-textfield__input" type="text" id="sample3">
-		    <label class="mdl-textfield__label" for="sample3">Correo Institucional</label>
-		  </div>
-		</form>
-	</div>
-
-	<div class="mdl-cell mdl-cell--3-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
-		    <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">
-		    <label class="mdl-textfield__label" for="sample2">TELEFONO N° 1</label>
-		    <span class="mdl-textfield__error">Input is not a number!</span>
-		  </div>
-		</form>
-	</div>
-
-	<div class="mdl-cell mdl-cell--3-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
-		    <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">
-		    <label class="mdl-textfield__label" for="sample2">TELEFONO N°2</label>
-		    <span class="mdl-textfield__error">Input is not a number!</span>
-		  </div>
-		</form>
-	</div>
-</div>
-
-<div class="mdl-grid">
-	<div class="mdl-cell mdl-cell--3-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-		    <input class="mdl-textfield__input" type="text" id="sample3">
-		    <label class="mdl-textfield__label" for="sample3">Encargado Jurídico</label>
-		  </div>
-		</form>
-	</div>
-
-	<div class="mdl-cell mdl-cell--3-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
-		    <input class="mdl-textfield__input" type="text" id="sample1">
-		    <label class="mdl-textfield__label" for="sample1">NOMBRE</label>
-		  </div>
-		</form>
-	</div>
-
-	<div class="mdl-cell mdl-cell--3-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
-		    <input class="mdl-textfield__input" type="text" id="sample1">
-		    <label class="mdl-textfield__label" for="sample1">APELLIDO</label>
-		  </div>
-		</form>
-	</div>
-
-	<div class="mdl-cell mdl-cell--3-col">
-		<!-- Numeric Textfield -->
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
-		    <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">
-		    <label class="mdl-textfield__label" for="sample2">CEDULA</label>
-		    <span class="mdl-textfield__error">Input is not a number!</span>
-		  </div>
-		</form>
-	</div>
-</div>
-
-
-<div class="mdl-grid">
-	<div class="mdl-cell mdl-cell--6-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-		    <input class="mdl-textfield__input" type="text" id="sample3">
-		    <label class="mdl-textfield__label" for="sample3">Correo Institucional</label>
-		  </div>
-		</form>
-	</div>
-
-	<div class="mdl-cell mdl-cell--3-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
-		    <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">
-		    <label class="mdl-textfield__label" for="sample2">TELEFONO N° 1</label>
-		    <span class="mdl-textfield__error">Input is not a number!</span>
-		  </div>
-		</form>
-	</div>
-
-	<div class="mdl-cell mdl-cell--3-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
-		    <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">
-		    <label class="mdl-textfield__label" for="sample2">TELEFONO N°2</label>
-		    <span class="mdl-textfield__error">Input is not a number!</span>
-		  </div>
-		</form>
-	</div>
-</div>
-
-<div class="mdl-grid">
-	<div class="mdl-cell mdl-cell--3-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-		    <input class="mdl-textfield__input" type="text" id="sample3">
-		    <label class="mdl-textfield__label" for="sample3">Encargado de Operaciones</label>
-		  </div>
-		</form>
-	</div>
-
-	<div class="mdl-cell mdl-cell--3-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
-		    <input class="mdl-textfield__input" type="text" id="sample1">
-		    <label class="mdl-textfield__label" for="sample1">NOMBRE</label>
-		  </div>
-		</form>
-	</div>
-
-	<div class="mdl-cell mdl-cell--3-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
-		    <input class="mdl-textfield__input" type="text" id="sample1">
-		    <label class="mdl-textfield__label" for="sample1">APELLIDO</label>
-		  </div>
-		</form>
-	</div>
-
-	<div class="mdl-cell mdl-cell--3-col">
-		<!-- Numeric Textfield -->
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
-		    <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">
-		    <label class="mdl-textfield__label" for="sample2">CEDULA</label>
-		    <span class="mdl-textfield__error">Input is not a number!</span>
-		  </div>
-		</form>
-	</div>
-</div>
-
-
-<div class="mdl-grid">
-	<div class="mdl-cell mdl-cell--6-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-		    <input class="mdl-textfield__input" type="text" id="sample3">
-		    <label class="mdl-textfield__label" for="sample3">Correo Institucional</label>
-		  </div>
-		</form>
-	</div>
-
-	<div class="mdl-cell mdl-cell--3-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
-		    <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">
-		    <label class="mdl-textfield__label" for="sample2">TELEFONO N° 1</label>
-		    <span class="mdl-textfield__error">Input is not a number!</span>
-		  </div>
-		</form>
-	</div>
-
-	<div class="mdl-cell mdl-cell--3-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
-		    <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">
-		    <label class="mdl-textfield__label" for="sample2">TELEFONO N°2</label>
-		    <span class="mdl-textfield__error">Input is not a number!</span>
-		  </div>
-		</form>
-	</div>
-</div>
 @stop
--- resources/views/Producto.blade.php
+++ resources/views/Producto.blade.php
@@ -2,7 +2,21 @@
 @section ('content')
 
 <h5>Registro de Productos</h5>
+
 <div class="mdl-grid">
+	<form action="#">
+		 <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+		    <select style="border-color: blue;">
+		    	<option>Seleccione Tipo de Producción</option>
+		    	<option>Semi - Terminado</option>
+		    	<option>Terminado</option>
+		    	<option>Sub Producto</option>
+		    </select>
+		</div>
+	</form>
+</div>
+
+<div class="mdl-grid" >
 	<div class="mdl-cell mdl-cell--4-col">
 		<form action="#">
 		  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
@@ -14,22 +28,29 @@
 
 	<div class="mdl-cell mdl-cell--4-col">
 		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
-		    <input class="mdl-textfield__input" type="text" id="sample1">
-		    <label class="mdl-textfield__label" for="sample1">Descripción</label>
-		  </div>
-		</form>
-	</div>
-
-	<div class="mdl-cell mdl-cell--4-col">
-		<form action="#">
-		  <div class="mdl-textfield mdl-js-textfield">
+		  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
 		    <input class="mdl-textfield__input" type="text" id="sample1">
 		    <label class="mdl-textfield__label" for="sample1">Unidad</label>
 		  </div>
 		</form>
 	</div>
 
+	<div class="mdl-cell mdl-cell--4-col">
+		<form action="#">
+		  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+		    <textarea class="mdl-textfield__input" type="text" rows= "1" id="sample5" ></textarea>
+		    <label class="mdl-textfield__label" for="sample5">Descripción</label>
+		  </div>
+		</form>
+	</div>
+
+</div>
+
+<div class="mdl-grid"
+	<div class="mdl-cell mdl-cell--4-col-offset mdl-cell--4-col">
+		<button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored">Agregar</button>
+	</div>
+</div>
 
 
 @stop
