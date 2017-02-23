c62bb5dbc9ee8fac8dc9e9b9c728c6ad773bae9e
Edwin Diaz <edwinddiazd@gmail.com>
2017-02-23 00:33:09 -0400

    modificacion del home como pantalla de entrada al sistema

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,42 @@
-@extends ('main')
-@section ('content')
-<div>
-<p>contenido home en el beta esta verga no ve</p>
-<h2>modificación </h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>SEIPBES</title>
+	<link rel="stylesheet" href="material.min.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body class="mdl-color--grey-50">
+<div class="mdl-grid">
+	<div class="mdl-layout-spacer"></div>
+	<div class="mdl-cell mdl-cell--4-col-offset mdl-cell--4-col mdl-shadow--3dp mdl-color--grey-100" style="margin-top: 15%;">
+		<div class="mdl-layout-spacer "></div>
+		<div class="mdl-color--indigo-400" style="padding-top: 10px;padding-bottom: 10px;padding-left: 10px;">
+      		<span class="mdl-layout-title mdl-color-text--white">SEIPBES</span>
+      		<div class="mdl-layout-spacer"></div>
+		</div>
+		<div>
+			<form action="#">
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" style="margin-left: 40px;">
+					<input class="mdl-textfield__input" type="text" id="user_input">
+					<label class="mdl-textfield__label" for="user_input">Usuario</label>
+				</div>
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" style="margin-left: 40px;">
+					<input class="mdl-textfield__input" type="text" id="pwd_input">
+					<label class="mdl-textfield__label" for="pwd_input">Password</label>
+				</div>
+				<div class="mdl-grid" style="margin-left: 15%;">
+					<div class="mdl-cell mdl-cell--4-col-offset mdl-cell--4-col">
+						<button type="submit" class="mdl-button mdl-js-button mdl-button--primary">Ingresar</button>
+					</div>
+					<div class="mdl-cell mdl-cell--4-col"><button type="submit" class="mdl-button mdl-js-button mdl-button--accent">Registrarse</button></div>
+				</div>
+			</form>
+		</div>
+		<div class="mdl-layout-spacer"></div>
+		</div>
+	<div class="mdl-layout-spacer"></div>
 </div>
-@stop
+</body>
+<script src="material.min.js"></script>
+</html>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,10 +7,10 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body>
-@include('partials._navbar')
+
 <main class="mdl-layout__content">
     <div class="mdl-grid mdl-color--red-100" style="max-width: 1080px;">
-		<div class="mdl-cell mdl-cell--3-offset mdl-cell--9-col">
+		<div class="mdl-cell mdl-cell--3-offset-desktop mdl-cell--9-col-desktop">
 			@yield ('content')<!-- Your content goes here -->
 		</div>
 
