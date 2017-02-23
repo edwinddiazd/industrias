0a6471e21b36061194bb928dc46fe5cc815bab9b
Edwin Diaz <edwinddiazd@gmail.com>
2017-02-23 16:06:34 -0400

    para q a made se le vea el registro

--- resources/views/Registro_Industrias.blade.php
+++ resources/views/Registro_Industrias.blade.php
@@ -1,4 +1,59 @@
 @extends ('main2')
 @section ('content')
-
+<div class="mdl-grid">
+	<div class="mdl-layout-spacer"></div>
+	<div class="mdl-cell mdl-cell--2-col-offset mdl-cell--8-col mdl-shadow--3dp mdl-color--grey-100" style="margin-top: 15%;">
+		<div class="mdl-layout-spacer "></div>
+		<div class="mdl-color--indigo-400" style="padding-top: 10px;padding-bottom: 10px;padding-left: 10px;">
+      		<span class="mdl-layout-title mdl-color-text--white">SEIPBES</span>
+      		<div class="mdl-layout-spacer"></div>
+		</div>
+		<div class="mdl-grid">
+			<div class="mdl-cell mdl-cell--4-col">
+				<form action="#">
+					<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" >
+						<input class="mdl-textfield__input" type="text" id="sample1">
+			    		<label class="mdl-textfield__label" for="sample1">INDUSTRIA</label>
+					</div>
+				</form>
+			</div>
+			<div class="mdl-cell mdl-cell--4-col">
+				<form action="#">
+					<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" style="margin-left: 40px;">
+						<textarea class="mdl-textfield__input" type="text" rows= "1" id="sample2" ></textarea>
+			    		<label class="mdl-textfield__label" for="sample2">DIRECCION</label>
+					</div>
+				</form>
+			</div>
+			<div class="mdl-grid">
+				<div class="mdl-cell mdl-cell--4-col">
+					<form action="#">
+					  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" style="margin-left: -40px;" >
+					    <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample3" >
+					    <label class="mdl-textfield__label" for="sample3">TELEFONO N°1</label>
+					    <span class="mdl-textfield__error">Input is not a number!</span>
+					  </div>
+					</form>
+				</div>
+				<div class="mdl-cell mdl-cell--4-col">
+					<form action="#">
+					  <div class="mdl-textfield mdl-js-textfield">
+					    <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">
+					    <label class="mdl-textfield__label" for="sample2">TELEFONO N°2</label>
+					    <span class="mdl-textfield__error">Input is not a number!</span>
+					  </div>
+					</form>
+				</div>
+			</div>
+				<div class="mdl-grid" style="margin-left: 15%;">
+					<div class="mdl-cell mdl-cell--4-col-offset mdl-cell--4-col">
+						<button type="submit" class="mdl-button mdl-js-button mdl-button--primary">Registrar</button>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="mdl-layout-spacer"></div>
+		</div>
+	<div class="mdl-layout-spacer"></div>
+</div>
 @stop
