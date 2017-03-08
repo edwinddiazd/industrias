9b9370018a1bf877f11410ccba640c01c6ae75bd
Edwin Diaz <edwinddiazd@gmail.com>
2017-03-07 21:57:34 -0400

    navbar arreglado

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,12 +7,12 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body class="mdl-color--grey-50">
-@include('partials._navbar')
-<main class="mdl-layout__content">
-    @include('partials._messages')
-	@yield ('content')
-</main>
-</div>
+	@include('partials._navbar')
+	<main class="mdl-layout__content">
+	    @include('partials._messages')
+		@yield ('content')
+	</main>
+	</div>
 </body>
 <script src="material.min.js"></script>
 </html>
