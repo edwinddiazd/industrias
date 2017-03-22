c514a514a8789ba9acd96a1563840109096cace2
Edwin Diaz <edwinddiazd@gmail.com>
2017-03-21 21:26:35 -0400

    arreglos del login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,66 +1,43 @@
-@extends('layouts.app')
+@extends('main')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+<div class="mdl-grid">
+    <div class="mdl-layout-spacer"></div>
+    <div class="mdl-cell mdl-cell--4-col-offset mdl-cell--5-col mdl-shadow--3dp mdl-color--grey-100" style="margin-top: 10%;">
+        <div class="mdl-layout-spacer "></div>
+        <div class="mdl-color--indigo-A700" style="padding-top: 10px;padding-bottom: 10px;padding-left: 10px;">
+            <span class="mdl-layout-title mdl-color-text--white">Bienvenido</span>
+            <div class="mdl-layout-spacer"></div>
+        </div>
+        <div class="mdl-grid">
+            <div class="mdl-cell mdl-cell--2-col-offset mdl-cell--8-col">
+                    <form role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" style="margin-left: 40px;">
+                            <input class="mdl-textfield__input" type="text" id="user_input" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
+                            <label class="mdl-textfield__label" for="user_input">Usuario</label>
+                            <span class="mdl-textfield__error">Debes ingresar tu correo</span>
                         </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" style="margin-left: 40px;">
+                            <input class="mdl-textfield__input" type="password" id="pwd_input" pattern=".{6,}">
+                            <label class="mdl-textfield__label" for="pwd_input">Password</label>
+                            <span class="mdl-textfield__error">El password debe tener al menos 6 caracteres</span>
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
+                        <div class="mdl-grid">
+                            <div class="mdl-cell mdl-cell--4-col">
+                                <button type="submit" class="mdl-button mdl-js-button mdl-button--primary">Ingresar</button>
                             </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i> Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+                            <div class="mdl-cell mdl-cell--8-col-offset mdl-cell--4-col" style="margin-left: 15%;">
+                            <button class="mdl-button mdl-js-button mdl-button--accent">
+                            <a href="Registro" style="text-decoration: none;">Registrarse</a>
+                            </button>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
+    <div class="mdl-layout-spacer"></div>
     </div>
-</div>
 @endsection
