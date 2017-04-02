f3620920fa36196a6773bea766e04094e2424344
madeleind <madeleind.reyes@gmail.com>
2017-04-01 22:27:53 -0400

    home2

---
+++ resources/views/home2.blade.php
@@ -0,0 +1,29 @@
+@extends ('main')
+
+@section ('title','|')
+
+@section ('content')
+
+<div class="mdl-grid">
+<div class="mdl-layout-spacer"></div>
+	<div class="mdl-cell mdl-cell--10-col mdl-shadow--3dp mdl-color--grey-100">
+		<div class="mdl-grid">
+		<div class="mdl-layout-spacer"></div>
+			<div class="mdl-cell mdl-cell--1-col mdl-cell--10-col">
+			<div><h4>Datos Personal</h4></div>
+			<table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+  				<thead>
+
+  				</thead>
+  				<tbody>
+				    
+  				</tbody>
+			</table>
+			</div>
+			<div class="mdl-layout-spacer"></div>
+		</div>
+	</div>
+	<div class="mdl-layout-spacer"></div>
+</div>
+
+@stop
