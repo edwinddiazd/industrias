5f8df0a6a2be51daf849776efba0f89a8a81f244
Edwin Diaz <edwinddiazd@gmail.com>
2017-04-08 15:52:54 -0400

    modificaciones de usuario

--- app/User.php
+++ app/User.php
@@ -23,7 +23,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','industrias_id',
     ];
 
     /**
